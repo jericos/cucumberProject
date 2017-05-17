0eb843a05760461f84126c75aa28044476c639a2
jerico <jsegura@belatrixsf.com>
2017-05-17 16:17:20 -0500

    add new

--- src/test/java/Transformation/EmailTransform.java
+++ src/test/java/Transformation/EmailTransform.java
@@ -8,6 +8,6 @@ public class EmailTransform extends Transformer<String> {
 
     @Override
     public String transform(String userName){
-        return userName.concat("ea.com");
+        return userName.concat("mail.com");
     }
 }
