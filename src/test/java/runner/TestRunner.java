b2c1ee8b6dbffedbf2363e900e1b9a2bfcf8f575
js <jerico_s@hotmail.com>
2017-05-17 08:18:04 -0500

    Add new changes

---
+++ src/test/java/runner/TestRunner.java
@@ -0,0 +1,10 @@
+package runner;
+
+import cucumber.api.CucumberOptions;
+import org.junit.runner.RunWith;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = {"src/test/java/Features"}, glue="Steps")
+public class TestRunner {
+}
